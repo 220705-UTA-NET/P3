44fa3a2c0faeb359e9c2db59c49f8127d5238d4a
BuildTools <cwchrono@gmail.com>
2022-08-24 09:47:01 -0700

    fixed dependencies

--- server/server.Model/Program.cs
+++ server/server.Model/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace server.Model
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
