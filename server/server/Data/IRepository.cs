e5cce253057893eef9325ba9ec0c92b473496c97
Dbeidels <dbeidels@macalester.edu>
2022-08-24 11:13:41 -0500

    fixed typo

--- server/server/Data/IRepository.cs
+++ server/server/Data/IRepository.cs
@@ -6,6 +6,6 @@ namespace server.Data
     public interface IRepository
     {
         Task<IEnumerable<Customer>> GetAllCustomersAsync();
-        Task<StatusCodeResult> UpdateCustomerAsync(int id, string firstname, string lastname, string email, int phonenumber, string password);
+        Task<StatusCodeResult> UpdateCustomerAsync(int id, string email, int phonenumber, string password);
     }
 }
