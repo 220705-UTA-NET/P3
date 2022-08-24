2edbf55c8b6f0ca611d73e8f7e90a0daf7d9d925
arthurgao697 <arthurgao697@revature.net>
2022-08-24 09:22:15 -0700

    Base V2 of Data Models

---
+++ server/Server_DataModels/DMODEL_Request.cs
@@ -0,0 +1,32 @@
+﻿namespace Server_DataModels
+{
+    public class DMODEL_Request
+    {
+        // FIELDS
+        public int request_id           { get; set; }
+        public int sender_id            { get; set; }
+        public int reciever_id          { get; set; }
+        public float amount             { get; set; }
+        public DateTime time            { get; set; }
+        public int account_sender_id    { get; set; }
+        public int account_reciever_id  { get; set; }
+        public int status               { get; set; }
+
+        // CONSTRUCTORS
+        public DMODEL_Request() { }
+
+        public DMODEL_Request(int request_id, int sender_id, int reciever_id, float amount, DateTime time, int account_sender_id, int account_reciever_id, int status)
+        {
+            this.request_id = request_id;
+            this.sender_id = sender_id;
+            this.reciever_id = reciever_id;
+            this.amount = amount;
+            this.time = time;
+            this.account_sender_id = account_sender_id;
+            this.account_reciever_id = account_reciever_id;
+            this.status = status;
+        }
+
+        // METHODS
+    }
+}
---
+++ server/Server_DataModels/DMODEL_Transaction.cs
@@ -0,0 +1,28 @@
+﻿namespace Server_DataModels
+{
+    public class DMODEL_Transaction
+    {
+        // FIELDS
+        public int transaction_id       { get; set; }
+        public int account_id           { get; set; }
+        public DateTime time            { get; set; }
+        public float amount               { get; set; }
+        public bool type                 { get; set; }
+        public bool completion_status   { get; set; }
+
+        // CONSTRUCTORS
+        public DMODEL_Transaction() { }
+
+        public DMODEL_Transaction(int transaction_id, int account_id, DateTime time, float amount, bool type, bool completion_status)
+        {
+            this.transaction_id = transaction_id;
+            this.account_id = account_id;
+            this.time = time;  
+            this.amount = amount;
+            this.type = type;
+            this.completion_status = completion_status;
+        }
+
+        // METHODS
+    }
+}
