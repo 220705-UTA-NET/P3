597d66692991af24d55698c0b3d04f96bd2cd32f
Dbeidels <dbeidels@macalester.edu>
2022-08-25 14:30:08 -0500

    fixed merging conflict

--- server/server/Data/IRepository.cs
+++ server/server/Data/IRepository.cs
@@ -6,11 +6,7 @@ namespace server.Data
     public interface IRepository
     {
         Task<Customer> GetCustomerAsync(int id);
-<<<<<<< HEAD
-        Task UpdateCustomerAsync(int id, string email, int phonenumber, string password);
-        Task<IEnumerable<Account>> GetCustomerAccountsAsync(int id);
-=======
         Task UpdateCustomerAsync(int id, string email, string phonenumber, string password);
->>>>>>> eaa502da02347e28f84e16560b93767cb859e813
+        Task<IEnumerable<Account>> GetCustomerAccountsAsync(int id);
     }
 }
