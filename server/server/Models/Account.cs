10381033fd8c4a09b93990062a4c126565bbc457
Dbeidels <dbeidels@macalester.edu>
2022-08-25 14:24:25 -0500

    made new model for account

---
+++ server/server/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace server.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string Type { get; set; }
+        public float Balance { get; set; }
+        public int CustomerId { get; set;}
+        public Account(int accountid, string type, float balance, int customerid)
+        {
+            AccountId = accountid;
+            Type = type;
+            Balance = balance;
+            CustomerId = customerid;
+        }
+    }
+}
