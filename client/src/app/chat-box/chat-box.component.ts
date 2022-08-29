132f6c6f8d17055abe64c4a0813b13e7902d39f9
BuildTools <cwchrono@gmail.com>
2022-08-28 22:02:50 -0700

    added chatbox functionality: send messages, message history

--- client/src/app/chat-box/chat-box.component.ts
+++ client/src/app/chat-box/chat-box.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ChatMessage } from '../models/ChatDTO';
 
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.css']
 })
+
+
 export class ChatBoxComponent implements OnInit {
 
+  user : string = 'Lance';
+  messages : ChatMessage[] = [];
+  sendContents : string = '';
   constructor() { }
 
   ngOnInit(): void {
+
+    let testMessages: ChatMessage[] = [
+      {
+        user: 'Lance',
+        message: 'hello'
+      },
+      {
+        user: 'Kadin',
+        message: 'hello to you too'
+      },
+      {
+        user: 'Lance',
+        message: 'cool'
+      },
+      {
+        user: 'Lance',
+        message: 'overflow'
+      },
+      {
+        user: 'Onandi',
+        message: 'overflow?'
+      },      
+      {
+        user: 'Lance',
+        message: 'overflow.'
+      },
+      {
+        user: 'Lance',
+        message: 'overflow'
+      },
+      {
+        user: 'Onandi',
+        message: 'overflow?'
+      },      
+      {
+        user: 'Lance',
+        message: 'overflow.'
+      }
+    ];
+
+    testMessages.forEach((msg: ChatMessage) => {
+      this.messages.push(msg);
+    });
   }
 
+  submitMessage(){
+    let newMessage: ChatMessage = {
+      user: this.user,
+      message: this.sendContents
+    }
+    console.log(this.user + ": " + this.sendContents);
+    this.messages.push(newMessage); 
+  }
 }
