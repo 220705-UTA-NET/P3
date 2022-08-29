9d48e7c863f3be1f7ba17892c972332864b519db
James Beitz <108765503+BeitzJR@users.noreply.github.com>
2022-08-29 13:46:09 -0700

    Dev (#12)
    * Create README.md
    
    Created Readme File
    
    * Update README.md
    
    * Update README.md
    
    * Base V2 of Data Models
    
    * Removed Weather Forecast default files
    
    * V1.0 of Transaction History, need testings - awaiting dummy DB
    
    * Removing old version of DataModels
    
    * Updated controller and Dmoodels to handle mock database
    
    * Updated DMODEL_Transaction to account for new columns in database
    
    * adding spa
    
    * Added Money Transfer Function (V1)<- Included Base Deposit and Withdraw
    
    Co-authored-by: arthurgao697 <arthurgao697@revature.net>
    Co-authored-by: NymStark <hau.q.nguyen@gmail.com>
    Co-authored-by: jdelacruz96 <92655139+jdelacruz96@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ I am a read me file. Please remember to fill me in before the project is done.
 
 Also my brother the Startup Documentation.
 
+After finding out about the WIKI I am useless!!
+
+The Dwarves are taking up residence here. 
+
--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,3 +1,8 @@
+using server_Database;
+
+// TEMP connection string for internal testing
+string DB_connectionString = await File.ReadAllTextAsync(@"./../../../connectionString_P3.txt");
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IRepository>(sp => new SQLRepository(DB_connectionString, sp.GetRequiredService<ILogger<SQLRepository>>()));
 
 var app = builder.Build();
 
