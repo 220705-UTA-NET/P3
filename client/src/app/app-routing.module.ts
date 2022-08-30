d03b26f5b6d4b60627465ef183f1009c764b9e99
derickx1 <derickwxie@gmail.com>
2022-08-30 14:02:01 -0700

    Changed naming for user-profile and added unit tests

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckingAccountComponent } from './checking-account/checking-account.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [{ path: "\customer", component: UserProfileComponent, }];
+const routes: Routes = [{ path: "\customer", component: UserProfileComponent, },
+                        { path: "\accounts", component: CheckingAccountComponent, }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/user-profile/user-profile.component.ts
+++ client/src/app/user-profile/user-profile.component.ts
@@ -2,10 +2,19 @@ import { Component } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 export interface Customer {
+<<<<<<< Updated upstream
   id: number,
   name: string,
   email: string,
   phone: number
+=======
+  customer_id: number,
+  first_name: string,
+  last_name: string,
+  email: string,
+  phone: string,
+  password: string
+>>>>>>> Stashed changes
 }
 
 @Component({
@@ -17,9 +26,21 @@ export class UserProfileComponent {
 
   constructor(private http: HttpClient) { }
 
+<<<<<<< Updated upstream
   name = 'name';
   email = 'email';
   phone = 0;
+=======
+  customer: Customer = {
+    customer_id: 0,
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+    password: ""
+  };
+  customerSet: Boolean = false;
+>>>>>>> Stashed changes
 
   getCustomer(value: any) {
     this.http.get(`https://localhost:7249/userprofile`, {
@@ -36,5 +57,33 @@ export class UserProfileComponent {
     
     console.log("In getCustomer: " + value);
   }
+<<<<<<< Updated upstream
+=======
+
+  editFirstName(event: any) {
+    this.customer.first_name = event.target.value
+  }
+
+  editLastName(event: any) {
+    this.customer.last_name = event.target.value
+  }
+
+  editEmail(event: any) {
+    this.customer.email = event.target.value
+  }
+
+  editPhoneNumber(event: any) {
+    this.customer.phone = event.target.value
+  }
+
+  editPassword(event: any) {
+    this.customer.password = event.target.value
+  }
+
+  submitChanges() {
+    console.log(this.customer);
+    this.http.put('https://localhost:7249/userprofile', this.customer).subscribe()
+  }
+>>>>>>> Stashed changes
     
 }
