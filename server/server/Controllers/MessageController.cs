be81ec96803b6a3c358504f2bc836113242bbb01
BuildTools <cwchrono@gmail.com>
2022-09-01 09:29:37 -0700

    added controller

---
+++ server/server/Controllers/MessageController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using server.Data;
+using server.Model;
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : Controller
+    {
+        private readonly IRepository _repo;
+        private readonly ILogger<MessageController> _logger;
+        public MessageController(IRepository repo, ILogger<MessageController> logger) {
+            _repo = repo;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TicketDTO>>> GetAllTickets() {
+            IEnumerable<TicketDTO> tickets;
+            try
+            {
+                tickets = await _repo.LoadAllTickets();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode(500);
+            }
+
+            return tickets.ToList();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDTO>>> GetAllMessagesbyTicket(string key = null)
+        {
+            IEnumerable<MessageDTO> messages;
+            try
+            {
+                messages = await _repo.LoadAllMessagesbyTicket(key);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode(500);
+            }
+
+            return messages.ToList();
+        }
+    }
+}
