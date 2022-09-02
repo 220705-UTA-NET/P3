225a1073d038a179ddec6d56b1414095619663f3
alejandroh136 <108765499+alejandroh136@users.noreply.github.com>
2022-09-02 11:54:47 -0700

    Team gold (#32)
    * added server stuff
    
    * added server stuff
    
    * added method to insert a budget
    
    * bran changes
    
    * server changes
    
    * started xUnit testing
    
    * added the budget front-end stuff |- Unable to post budget
    
    * added date to budget | able to send to backend and backend is able to insert to database
    
    * able to update a budget
    
    * testing update
    
    * added project references. able to run server now
    
    Co-authored-by: Brandon <figueredo407@gmail.com>
    Co-authored-by: Jonathan De La Cruz <jdelacruznode@gmail.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { BudgetItemComponent } from './budget-item/budget-item.component';
+import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import { AddBudgetComponent } from './add-budget/add-budget.component';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LandingComponent } from './landing/landing.component';
@@ -11,6 +15,9 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
+    BudgetListComponent,
+    BudgetItemComponent,
+    AddBudgetComponent,
     NavBarComponent,
     HeaderComponent,
     LandingComponent,
@@ -18,6 +25,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using server_Database;
+using server.Data;
 
 // TEMP connection string for internal testing
 string? DB_connectionString = Environment.GetEnvironmentVariable("CONN");
@@ -12,6 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IBudgetRepository>(sp => new SQLBudgetRepository(DB_connectionString));
 
 builder.Services.AddSingleton<IRepository>(sp => new SQLRepository(DB_connectionString, sp.GetRequiredService<ILogger<SQLRepository>>()));
 string MyAllowAllOrgins = "_myAllowAllOrigins";
