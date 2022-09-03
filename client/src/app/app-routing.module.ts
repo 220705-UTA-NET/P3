c0c553a776bb108c98639778d80b97ec72296a43
unknown <alejandro136@revature.net>
2022-09-03 06:33:32 -0700

    merged some files form dev to Team_Gold

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BudgetListComponent } from './budget-calculator/budget-list/budget-list.component';
-import { AddBudgetComponent } from './budget-calculator/add-budget/add-budget.component';
-import { LandingComponent } from './landing/landing.component';
-
+import { ChatboxComponent } from './chatbox/chatbox.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { AddBudgetComponent } from './add-budget/add-budget.component';
 const routes: Routes = [
-  { path: 'budget-calculator', component: BudgetListComponent },
-  { path: '', component: LandingComponent },
-  // { path: '', component: BudgetListComponent },
-  // {path:'budget', component:BudgetListComponent},
-  {path:'budget/addnew', component:AddBudgetComponent}
+  {path:'', component:BudgetListComponent},
+  {path:'budget', component:BudgetListComponent},
+  {path:'budget/addnew', component:AddBudgetComponent},
+  {path: "chatbox", component: ChatboxComponent}
 ]
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChatboxComponent } from './chatbox/chatbox.component';
+import { ReactiveFormsModule  } from '@angular/forms';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BudgetListComponent } from './budget-list/budget-list.component';
+import { BudgetItemComponent } from './budget-item/budget-item.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
+import { AddBudgetComponent } from './add-budget/add-budget.component';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LandingComponent } from './landing/landing.component';
 import { FooterComponent } from './footer/footer.component';
 
-import { BudgetListComponent } from './budget-calculator/budget-list/budget-list.component';
-import { BudgetItemComponent } from './budget-calculator/budget-item/budget-item.component';
-import { AddBudgetComponent } from './budget-calculator/add-budget/add-budget.component';
-
 @NgModule({
   declarations: [
     AppComponent,
+    ChatboxComponent,
+    ChatBoxComponent,
+    AddBudgetComponent,
     BudgetListComponent,
     BudgetItemComponent,
-    AddBudgetComponent,
     NavBarComponent,
     HeaderComponent,
     LandingComponent,
@@ -26,9 +30,13 @@ import { AddBudgetComponent } from './budget-calculator/add-budget/add-budget.co
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
