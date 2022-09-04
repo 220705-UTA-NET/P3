a44ddfea7f0a85709d79d35279489765a1e7b352
alejandroh136 <108765499+alejandroh136@users.noreply.github.com>
2022-09-04 01:58:07 -0700

    Update app-routing.module.ts
    change the route to the component. should be able to build after this change.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatboxComponent } from './chatbox/chatbox.component';
-import { BudgetListComponent } from './budget-list/budget-list.component';
-import { AddBudgetComponent } from './add-budget/add-budget.component';
+import { BudgetListComponent } from './budget-calculator/budget-list/budget-list.component';
+import { AddBudgetComponent } from './budget-calculator/add-budget/add-budget.component';
 const routes: Routes = [
   {path:'', component:BudgetListComponent},
   {path:'budget', component:BudgetListComponent},
