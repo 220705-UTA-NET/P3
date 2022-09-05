42ada9e41c530124f88109d0bf5b7f785623498f
alejandroh136 <108765499+alejandroh136@users.noreply.github.com>
2022-09-05 09:54:15 -0700

    Update app-routing.module.ts
    added landing to routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChatboxComponent } from './chatbox/chatbox.component';
 import { BudgetListComponent } from './budget-calculator/budget-list/budget-list.component';
 import { AddBudgetComponent } from './budget-calculator/add-budget/add-budget.component';
+import { HeaderComponent } from './header/header.component';
 const routes: Routes = [
-  {path:'', component:BudgetListComponent},
+  {path:'', component:HeaderComponent},
   {path:'budget', component:BudgetListComponent},
   {path:'budget/addnew', component:AddBudgetComponent},
   {path: "chatbox", component: ChatboxComponent}
