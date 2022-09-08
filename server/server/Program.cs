bfc3c2af6bc399a7326e15bb9f9f5ad0e21b58af
jdelacruz96 <92655139+jdelacruz96@users.noreply.github.com>
2022-09-08 10:37:53 -0400

    Update Program.cs

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
     builder =>
     {
         builder.AllowAnyMethod().AllowAnyHeader()
-            .WithOrigins("https://misty-api-dev2.azurewebsites.net")
+            .WithOrigins("*")
             .AllowCredentials();
     }));
 
