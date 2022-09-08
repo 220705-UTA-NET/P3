f8fe1478109fd37fe3880b05a779d9e2cdffea1b
James Beitz <108765503+BeitzJR@users.noreply.github.com>
2022-09-07 20:04:42 -0700

    Made fields nullable

--- server/server.Model/TeamCopper_Customer.cs
+++ server/server.Model/TeamCopper_Customer.cs
@@ -9,12 +9,12 @@ namespace server.Model
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
 
         public Customer() { }
 
